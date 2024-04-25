2f0d23292a5f9ecf01797cb58b5d80b0d406b9ea
andriy <bartush33@gmail.com>
2024-04-25 21:51:48 +0300

    HW

---
+++ js/task-3.js
@@ -0,0 +1,8 @@
+function getElementWidth(content, padding, border) {
+    const getElementWidth = content + padding * 2 + border * 2;
+    return getElementWidth;
+}
+console.log(getElementWidth(Number.parseFloat("50px"), Number.parseFloat("8px"), Number.parseFloat("4px"))); 
+console.log(getElementWidth(Number.parseFloat("60px"), Number.parseFloat("12px"), Number.parseFloat("8.5px"))); 
+console.log(getElementWidth(Number.parseFloat("200px"), Number.parseFloat("0px"), Number.parseFloat("0px"))); 
+
